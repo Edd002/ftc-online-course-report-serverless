71fd788b168a5712ef118866c364693254c4dd98
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-10-12 18:37:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FTC.OnlineCourseReport.Serverless
+Projeto Tech Challenge Online Course Report Serverless do Curso de Pós-Graduação Lato Sensu Arquitetura e Desenvolvimento em JAVA da Faculdade de Informática e Administração Paulista (FIAP)
---
+++ src/test/java/com/fiap/tech/challenge/online/course/report/serverless/FTCOnlineCourseReportServerlessApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fiap.tech.challenge.online.course.report.serverless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FTCOnlineCourseReportServerlessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
