9a7afd4747b41a4e4c34dfce719f15cbcd080207
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-11-20 19:23:52 -0300

    #Fase-4-configuracao-envio-email : Configuração de envio de email com mailtrap via SMTP

---
+++ src/main/java/fiap/tech/challenge/online/course/report/serverless/config/EmailConfig.java
@@ -0,0 +1,61 @@
+package fiap.tech.challenge.online.course.report.serverless.config;
+
+import fiap.tech.challenge.online.course.report.serverless.loader.ApplicationPropertiesLoader;
+
+import java.util.Properties;
+
+public class EmailConfig {
+
+    private final String host;
+    private final int port;
+    private final String sender;
+    private final String username;
+    private final String password;
+    private final boolean smtpAuth;
+    private final boolean starttlsEnable;
+    private final String sslProtocol;
+
+    public EmailConfig() {
+        Properties applicationProperties = ApplicationPropertiesLoader.loadProperties(getClass().getClassLoader());
+        this.host = applicationProperties.getProperty("application.mail.host");
+        this.port = Integer.parseInt(applicationProperties.getProperty("application.mail.port"));
+        this.sender = applicationProperties.getProperty("application.mail.sender");
+        this.username = applicationProperties.getProperty("application.mail.username");
+        this.password = applicationProperties.getProperty("application.mail.password");
+        this.smtpAuth = Boolean.parseBoolean(applicationProperties.getProperty("application.mail.smtp.auth"));
+        this.starttlsEnable = Boolean.parseBoolean(applicationProperties.getProperty("application.mail.smtp.starttls.enable"));
+        this.sslProtocol = applicationProperties.getProperty("application.mail.ssl.protocol");
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPort() {
+        return String.valueOf(port);
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String isSmtpAuth() {
+        return String.valueOf(smtpAuth);
+    }
+
+    public String isStarttlsEnable() {
+        return String.valueOf(starttlsEnable);
+    }
+
+    public String getSslProtocol() {
+        return sslProtocol;
+    }
+}
--- src/main/java/fiap/tech/challenge/online/course/report/serverless/email/FTCOnlineCourseReportEmailDeliverService.java
+++ src/main/java/fiap/tech/challenge/online/course/report/serverless/email/FTCOnlineCourseReportEmailDeliverService.java
@@ -1,5 +1,6 @@
 package fiap.tech.challenge.online.course.report.serverless.email;
 
+import fiap.tech.challenge.online.course.report.serverless.config.EmailConfig;
 import fiap.tech.challenge.online.course.report.serverless.payload.FeedbackReportRequest;
 import fiap.tech.challenge.online.course.report.serverless.payload.FeedbackReportResponse;
 
@@ -10,32 +11,35 @@ import java.util.Properties;
 
 public class FTCOnlineCourseReportEmailDeliverService {
 
+    // CONTINUAR:
+    // (V) TROCAR EMAIL DE ENVIO, UMA VEZ QUE ADMIN FIAP IMPEDE ENVIOS (SALVAR SENHA EM VAULT/VARIÁVEL DE AMBIENTE) -> UTILIZAR https://mailtrap.io/ [CRIAR CONTA UTILIZANDO GitHub]
+    // () ADICIONAR CONFIGS DE EMAIL EM VARIÁVEIS DE AMBIENTE/VAULT
+    // () CONFIGURAR CHAMADA PARA ESTE LAMBDA EM ftc-online-course-feedback-receiver-serverless PARA FEEDBACKS URGENTES
+    // () TESTAR FLUXO COMPLETO -> BUSCA DE FEEDBACK, ENVIA EMAIL E CADASTRO EM t_urgent_email_notification
     public void sendEmailUrgentFeedback(FeedbackReportRequest feedbackReportRequest, FeedbackReportResponse feedbackReportResponse) {
+        EmailConfig emailConfig = new EmailConfig();
         try {
-            final String userName = "rm361276@fiap.com.br";
-            final String password = "123";
-
             Properties props = new Properties();
-            props.setProperty("mail.smtp.auth", "true");
-            props.setProperty("mail.smtp.starttls.enable", "true");
-            props.setProperty("mail.smtp.host", "outlook.office365.com");
-            props.setProperty("mail.smtp.port", "587");
-            props.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
+            props.setProperty("mail.smtp.host", emailConfig.getHost());
+            props.setProperty("mail.smtp.port", emailConfig.getPort());
+            props.setProperty("mail.smtp.auth", emailConfig.isSmtpAuth());
+            props.setProperty("mail.smtp.starttls.enable", emailConfig.isStarttlsEnable());
+            props.setProperty("mail.smtp.ssl.protocols", emailConfig.getSslProtocol());
 
             Session session = Session.getDefaultInstance(props,
                     new javax.mail.Authenticator() {
                         protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(userName, password);
+                            return new PasswordAuthentication(emailConfig.getUsername(), emailConfig.getPassword());
                         }
                     });
             session.setDebug(true);
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("rm361276@fiap.com.br"));
-            Address[] toUser = InternetAddress.parse("edduarddollima@gmail.com");
+            message.setFrom(new InternetAddress(emailConfig.getSender()));
+            Address[] toUser = InternetAddress.parse("emailadministrador@emai.com");
             message.setRecipients(Message.RecipientType.TO, toUser);
-            message.setSubject("Enviando email com JavaMail");
-            message.setText("Enviei este email utilizando JavaMail com minha conta Microsoft.");
+            message.setSubject("E-mail de notificação de feedback de aluno");
+            message.setText("Segue o relatório de feedback urgente do aluno de nome Nome Aluno e e-mail emailaluno@email.com:");
 
             Transport.send(message);
             System.out.println("E-mail enviado com sucesso.");
