6731a55e27bfe9b1543ee908b3a2bf0a6523d415
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-12-11 15:49:44 -0300

    #Fase-4-padronizacao-properties : Retirada de deserializar null para erros

--- src/main/java/fiap/tech/challenge/online/course/report/serverless/payload/record/error/ErrorResponse.java
+++ src/main/java/fiap/tech/challenge/online/course/report/serverless/payload/record/error/ErrorResponse.java
@@ -1,4 +1,7 @@
 package fiap.tech.challenge.online.course.report.serverless.payload.record.error;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(String message, String cause) {
 }
