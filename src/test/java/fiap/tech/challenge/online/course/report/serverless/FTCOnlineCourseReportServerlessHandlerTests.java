cc674a74c904667cacea44b6e112b1b6c855ded1
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-12-15 15:58:25 -0300

    #Fase-4-adaptacao-sqs : Refatoração de lambda de APIGatewayProxyRequestEvent para SQSEvent

--- src/test/java/fiap/tech/challenge/online/course/report/serverless/FTCOnlineCourseReportServerlessHandlerTests.java
+++ src/test/java/fiap/tech/challenge/online/course/report/serverless/FTCOnlineCourseReportServerlessHandlerTests.java
@@ -1,13 +1,14 @@
 package fiap.tech.challenge.online.course.report.serverless;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import fiap.tech.challenge.online.course.report.serverless.mock.TestContext;
 import fiap.tech.challenge.online.course.report.serverless.payload.HttpObjectMapper;
 import fiap.tech.challenge.online.course.report.serverless.payload.record.FeedbackReportRequest;
-import fiap.tech.challenge.online.course.report.serverless.mock.TestContext;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class FTCOnlineCourseReportServerlessHandlerTests {
 
@@ -15,11 +16,11 @@ class FTCOnlineCourseReportServerlessHandlerTests {
     void handleRequest_SendAndRegisterReportSuccess() {
         FTCOnlineCourseReportServerlessHandler handler = new FTCOnlineCourseReportServerlessHandler();
         TestContext context = new TestContext();
-        APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
-        request.setHttpMethod("POST");
-        request.setPath("/");
-        request.setBody(HttpObjectMapper.writeValueAsString(new FeedbackReportRequest("29ec6f1e-572a-4aad-b096-53225b77aaa8")));
-        APIGatewayProxyResponseEvent response = handler.handleRequest(request, context);
-        assertEquals(201, response.getStatusCode().intValue());
+        SQSEvent event = new SQSEvent();
+        SQSEvent.SQSMessage sqsMessage = new SQSEvent.SQSMessage();
+        sqsMessage.setBody(HttpObjectMapper.writeValueAsString(new FeedbackReportRequest("29ec6f1e-572a-4aad-b096-53225b77aaa8")));
+        event.setRecords(Collections.singletonList(sqsMessage));
+        Void voidResponse = handler.handleRequest(event, context);
+        assertNull(voidResponse);
     }
 }
